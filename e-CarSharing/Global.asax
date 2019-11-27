3be4e358bcebcfcd7bcf7feacf4f9dadfbbad35c
diogoacmarques <diogoacmarques@gmail.com>
2019-11-27 16:06:18 +0000

    Add project files.

---
+++ e-CarSharing/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="e_CarSharing.MvcApplication" Language="C#" %>
---
+++ e-CarSharing/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(e_CarSharing.Startup))]
+namespace e_CarSharing
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
